306a941763d57e3609bc867de3c08e81a0686991
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-14 19:22:21 -0400

    done

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,39 @@ public class HamletParser {
         return hamletData;
     }
 
-}
+    /*
+        // Create a pattern to be searched
+        Pattern pattern = Pattern.compile("geeks");
+
+        // Search above pattern in "geeksforgeeks.org"
+        Matcher m = pattern.matcher("geeksforgeeks.org");
+     */
+
+    /*
+      Pattern pattern = Pattern.compile(REGEX);
+
+      // get a matcher object
+      Matcher matcher = pattern.matcher(INPUT);
+      INPUT = matcher.replaceAll(REPLACE);
+
+     */
+
+    public String changeHamletToLeon( String str) {
+
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.replaceAll("Leon");
+    }
+
+    public String changeHoratioToTariq(String str) {
+
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.replaceAll("Tariq");
+    }
+    }
+
+
+
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,10 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.jar.JarOutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +20,67 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletText);
+        hamletText = matcher.replaceAll("Leon");
+        Assert.assertTrue(hamletText.contains("Leon"));
+    }
+
+    @Test
+    public void testChangeHamletToLeon1() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletText);
+        hamletText = matcher.replaceAll("Leon");
+        Assert.assertFalse(hamletText.contains("Hamlet"));
+
+    }
+
+    @Test
+    public void testChangedHamletToLeon2() {
+        String real = "O Hamlet, speak no more:";
+        String actual = hamletParser.changeHamletToLeon(real);
+        String expected = "O Leon, speak no more:";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangedHamletToLeon3() {
+        String real = "HAMLET\n" +
+                "O, that this too too solid flesh would melt";
+        String actual = hamletParser.changeHamletToLeon(real);
+        String expected = "Leon\n" +
+                "O, that this too too solid flesh would melt";
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(hamletText);
+        hamletText = matcher.replaceAll("Tariq");
+        Assert.assertTrue(hamletText.contains("Tariq"));
     }
 
     @Test
-    public void testFindHoratio() {
+    public void testChangeHaratioToTariq1() {
+        Pattern pattern = Pattern.compile("Haratio");
+        Matcher matcher = pattern.matcher(hamletText);
+        hamletText = matcher.replaceAll("Tariq");
+        Assert.assertFalse(hamletText.contains("Tariq"));
     }
 
     @Test
-    public void testFindHamlet() {
+    public void testChangeHoratioToTariq2() {
+        String real = "O good Horatio, I'll take the ghost's word for a";
+        String actual = hamletParser.changeHoratioToTariq(real);
+        String expected = "O good Tariq, I'll take the ghost's word for a";
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testChangeHoratioToTariq3() {
+        String real = "He may approve our eyes and speak to it.\n" + "HORATIO"; //case sensitive?
+        String actual = hamletParser.changeHoratioToTariq(real);
+        String expected = "He may approve our eyes and speak to it.\n" +  "Tariq";
+        Assert.assertEquals(expected, actual);
     }
 }
